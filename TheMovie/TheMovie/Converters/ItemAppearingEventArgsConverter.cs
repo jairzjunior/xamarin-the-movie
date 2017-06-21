3bc148a0fed0bab55c4adb591d8d3a128f849197
Jair Zeferino Júnior <jairzjunior@hotmail.com>
2017-06-21 13:17:39 -0300

    Added converters

---
+++ TheMovie/TheMovie/Converters/ItemAppearingEventArgsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TheMovie.Converters
+{
+	public class ItemVisibilityEventArgsConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var itemVisibilityEventArgs = value as ItemVisibilityEventArgs;
+			if (itemVisibilityEventArgs == null)
+			{
+				throw new ArgumentException($"Expected value to be of type {nameof(ItemVisibilityEventArgs)}", nameof(value));
+			}
+			return itemVisibilityEventArgs.Item;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ TheMovie/TheMovie/Converters/ItemTappedEventArgsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TheMovie.Converters
+{
+	public class ItemTappedEventArgsConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var itemTappedEventArgs = value as ItemTappedEventArgs;
+			if (itemTappedEventArgs == null)
+			{
+				throw new ArgumentException($"Expected value to be of type {nameof(ItemTappedEventArgs)}", nameof(value));
+			}
+			return itemTappedEventArgs.Item;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
